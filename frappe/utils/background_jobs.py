db232c5f42df51576c2af9c443448f0668ce34c5
Akhil Narang <me@akhilnarang.dev>
2024-11-19 16:06:39 +0530

    fix(background_jobs): init site if required for after_job hooks
    If a job has been retried, we'll enter the finally section after `frappe.destroy()` has been called
    
    Additionally, only run these on the "original" call, not on each retry call
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    (cherry picked from commit 090d57d97bfb4fcaa6db0a6a51267fd778116cf6)

--- frappe/utils/background_jobs.py
+++ frappe/utils/background_jobs.py
@@ -219,6 +219,9 @@ def execute_job(site, method, event, job_name, kwargs, user=None, is_async=True,
 		return retval
 
 	finally:
+		if not hasattr(frappe.local, "site"):
+			frappe.init(site)
+			frappe.connect()
 		for after_job_task in frappe.get_hooks("after_job"):
 			frappe.call(after_job_task, method=method_name, kwargs=kwargs, result=retval)
 
