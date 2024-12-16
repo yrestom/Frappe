72d1b9d8518743d3e82074b4a11eaeac92b69f85
Akhil Narang <me@akhilnarang.dev>
2024-12-16 17:18:48 +0530

    chore: resolve conflicts

--- .github/helper/install_dependencies.sh
+++ .github/helper/install_dependencies.sh
@@ -7,14 +7,5 @@ sudo apt update
 sudo apt remove mysql-server mysql-client
 sudo apt install libcups2-dev redis-server mariadb-client
 
-<<<<<<< HEAD
-wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
-sudo apt install ./wkhtmltox_0.12.6-1.focal_amd64.deb
-=======
-install_wkhtmltopdf() {
-  wget -q https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_amd64.deb
-  sudo apt install ./wkhtmltox_0.12.6.1-2.jammy_amd64.deb
-}
-install_wkhtmltopdf &
-echo "::endgroup::"
->>>>>>> f7a5e54650 (chore(ci): bump wkhtmltox from focal to jammy)
+wget -q https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_amd64.deb
+sudo apt install ./wkhtmltox_0.12.6.1-2.jammy_amd64.deb
