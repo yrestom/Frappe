4518b4562f2824dc0eb8becf337108006c4183e3
Ankush Menat <ankush@frappe.io>
2025-01-06 14:36:48 +0530

    test: add basic client cache tests

---
+++ frappe/tests/test_client_cache.py
@@ -0,0 +1,37 @@
+import time
+
+import frappe
+from frappe.tests import IntegrationTestCase
+
+TEST_KEY = "42"
+
+
+class TestClientCache(IntegrationTestCase):
+	def setUp(self) -> None:
+		frappe.client_cache.delete_value(TEST_KEY)
+		return super().setUp()
+
+	def test_client_cache_is_used(self):
+		frappe.client_cache.set_value(TEST_KEY, 42)
+		frappe.client_cache.get_value(TEST_KEY)
+		with self.assertRedisCallCounts(0):
+			frappe.client_cache.get_value(TEST_KEY)
+
+	def test_client_cache_is_updated_instantly_noloop(self):
+		val = frappe.generate_hash()
+		frappe.client_cache.set_value(TEST_KEY, val)
+		with self.assertRedisCallCounts(0):  # Locally set value should not be invalidated.
+			self.assertEqual(frappe.client_cache.get_value(TEST_KEY), val)
+
+	def test_invalidation_from_another_client_works(self):
+		val = frappe.generate_hash()
+		frappe.client_cache.set_value(TEST_KEY, val)
+		self.assertEqual(frappe.client_cache.get_value(TEST_KEY), val)
+
+		# frappe.cache is our "another client"
+		val = frappe.generate_hash()
+		frappe.cache.set_value(TEST_KEY, val)
+		time.sleep(0.5)
+
+		with self.assertRedisCallCounts(1):
+			self.assertEqual(frappe.client_cache.get_value(TEST_KEY), val)
