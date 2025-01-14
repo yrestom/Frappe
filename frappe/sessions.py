f963758a1d9d20a9f6839b4519bb4b2250bd0508
Ankush Menat <ankush@frappe.io>
2025-01-14 18:48:28 +0530

    perf: Avoid duplicate queries

--- frappe/sessions.py
+++ frappe/sessions.py
@@ -175,7 +175,7 @@ def get():
 		"default_path": get_default_path() or "",
 	}
 
-	bootinfo["desk_theme"] = frappe.db.get_value("User", frappe.session.user, "desk_theme") or "Light"
+	bootinfo["desk_theme"] = frappe.get_cached_value("User", frappe.session.user, "desk_theme") or "Light"
 	bootinfo["user"]["impersonated_by"] = frappe.session.data.get("impersonated_by")
 	bootinfo["navbar_settings"] = frappe.get_cached_doc("Navbar Settings")
 	bootinfo.has_app_updates = has_app_update_notifications()
