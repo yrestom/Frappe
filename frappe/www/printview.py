e49bbedb611616687b6a3beeda1d34c5fd68419f
Akhil Narang <me@akhilnarang.dev>
2025-01-24 16:51:16 +0530

    refactor(printview): first check for doc permissions, then for website
    This broke for some cases because people had doc permissions, but `has_website_permission()` returns False
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    (cherry picked from commit bbfb26aaab9f466a0e3edf62473ee827cec0c687)
    
    # Conflicts:
    #	frappe/www/printview.py

--- frappe/www/printview.py
+++ frappe/www/printview.py
@@ -349,13 +349,20 @@ def get_rendered_raw_commands(doc: str, name: str | None = None, print_format: s
 	}
 
 
+<<<<<<< HEAD
 def validate_print_permission(doc):
 	if frappe.has_website_permission(doc):
 		return
 
+=======
+def validate_print_permission(doc: "Document") -> None:
+>>>>>>> bbfb26aaab (refactor(printview): first check for doc permissions, then for website)
 	for ptype in ("read", "print"):
 		if frappe.has_permission(doc.doctype, ptype, doc):
 			return
+
+	if frappe.has_website_permission(doc):
+		return
 
 	if (key := frappe.form_dict.key) and isinstance(key, str):
 		validate_key(key, doc)
