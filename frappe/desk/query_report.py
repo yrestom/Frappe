709c74b0db546d7cc49c6866fbd8903ce287c0b0
Ejaaz Khan <67804911+iamejaaz@users.noreply.github.com>
2025-01-24 11:47:58 +0530

    refactor: modify error message for query report with insufficient permissions (#29884)
    * refactor: modify error message of not permitted report
    
    * refactor: show the name of the report whose permission is being checked
    
    (cherry picked from commit a4614da8052b226c2b213ecedd7e0c8f88992b5a)
    
    # Conflicts:
    #	frappe/desk/query_report.py

--- frappe/desk/query_report.py
+++ frappe/desk/query_report.py
@@ -49,7 +49,11 @@ def get_report_doc(report_name):
 
 	if not doc.is_permitted():
 		frappe.throw(
+<<<<<<< HEAD
 			_("You don't have access to Report: {0}").format(report_name),
+=======
+			_("You don't have access to Report: {0}").format(_(doc.name)),
+>>>>>>> a4614da805 (refactor: modify error message for query report with insufficient permissions (#29884))
 			frappe.PermissionError,
 		)
 
